432d3109a2d8af2aaf31884277daa8bcc79f7f14
hagari <hagari07@gmail.com>
2016-12-14 11:04:44 +0000

    Add produtcs

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+Product.create(name:'Green Tea' , price: 19.90 , details:'100% pure Green tea carefully chosen from the best highland plantations.' , quantity: 500)
+Product.create(name:'English Breakfast Tea' , price: 17.50 , details:'An uplifting blend of zesty Ceylon teas expertly combined with rich, robust Indian teas.' , quantity: 500)
+Product.create(name:'Earl Grey Tea' , price: 18.90, details:'Premium black tea perfectly infused with the alluring flavor and fragrance of bergamot.' , quantity: 500)
+Product.create(name:'Pure White Tea with Jasmine' , price: 21.00 , details:'Delicate, premium white tea subtly flavored with natural, sweet Arabian jasmin flowers.' , quantity: 500)
+Product.create(name:'Nana Mint with Ginger and Lemon' , price: 20.90 , details:'Warming ginger and the zest of fresh lemongrass beautifully compliment natural spearmint leaves.' , quantity: 500)
